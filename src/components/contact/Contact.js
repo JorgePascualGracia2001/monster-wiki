cf9adfeb476b94b5c6a81edc53dd57c3622d181d
Jorge Pascual <jorgepascualgracia@gmail.com>
2022-06-21 00:25:05 +0200

    contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,48 @@
 import "./Contact.scss";
-import Card from '../common/Card'
+import Card from "../common/Card";
+import { useState } from "react";
 
 function Contact() {
+  let [contactSent, setContactSent] = useState();
+
+  function sendContact(e){
+    setContactSent(!contactSent);
+    e.preventDefault();
+  };
+
+  if (contactSent){
+    return(
+      <Card className="card-main contact-form">
+        <Card className="card-section">
+          <form onSubmit={sendContact}>
+            <div className="form-component d-flex thanks-message">
+              <div>Gracias por ponerte en contacto con nosotros, revisaremos tu incidencia!</div>
+            </div>
+            <div className="d-flex flex-justify-center button-wrapper">
+              <button type="submit">Enviar otra incidencia</button>
+            </div>
+          </form>
+        </Card>
+      </Card>
+    )
+  }
   return (
-    <Card className="card-main">
-      <h2>CONTACTA CON NOSOTROS</h2>
+    <Card className="card-main contact-form">
+      <Card className="card-section">
+        <form onSubmit={sendContact}>
+          <div className="form-component d-flex">
+            <div>Asunto:</div>
+            <input></input>
+          </div>
+          <div className="form-component d-flex">
+            <div>Descripción:</div>
+            <textarea></textarea>
+          </div>
+          <div className="d-flex flex-justify-center button-wrapper">
+            <button type="submit">Enviar</button>
+          </div>
+        </form>
+      </Card>
     </Card>
   );
 }
